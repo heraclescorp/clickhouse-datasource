6c0448440067a595a06e31ba4a1cd22d5511a3c3
aven-mathew <mathew@aven.com>
2025-12-04 14:41:54 -0700

    deploy adjustments

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,11 @@ set +x
 # cd to location of this script https://stackoverflow.com/questions/6393551/what-is-the-meaning-of-0-in-a-bash-script
 cd "${0%/*}"
 
-yarn install
-yarn build
+npm install
+npm run build
+
+mage
 
 zip -vr aven-grafana-clickhouse-datasource.zip dist
 
-. ./loginToAwsAndSetupEnv.sh
 aws s3 cp aven-grafana-clickhouse-datasource.zip s3://aven-grafana-apps/aven-grafana-clickhouse-datasource.zip
